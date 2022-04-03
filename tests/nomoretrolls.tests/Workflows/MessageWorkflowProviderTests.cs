385bd762c90d5c7e19b5c159964e8a3d658ba8fa
tonycknight <tonycknight@users.noreply.github.com>
2022-04-03 21:21:03 +0100

    more tests

---
+++ tests/nomoretrolls.tests/Workflows/MessageWorkflowProviderTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using nomoretrolls.Commands;
+using nomoretrolls.Config;
+using nomoretrolls.Io;
+using nomoretrolls.Workflows;
+using NSubstitute;
+using Xunit;
+
+
+namespace nomoretrolls.tests.Workflows
+{
+    public class MessageWorkflowProviderTests
+    {
+        [Fact]
+        public void CreateBlacklistedUserWorkflow_ResultReturned()
+        {
+            var wff = Substitute.For<IMessageWorkflowFactory>();
+
+            var mwp = new MessageWorkflowProvider(wff);
+
+            var r = mwp.CreateBlacklistedUserWorkflow();
+
+            r.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CreateBlacklistedPersonalReplyWorkflow_ResultsReturned()
+        {
+            var wff = Substitute.For<IMessageWorkflowFactory>();
+
+            var mwp = new MessageWorkflowProvider(wff);
+
+            var r = mwp.CreateBlacklistedPersonalReplyWorkflow();
+
+            r.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void CreateShoutingWorkflow_ResultsReturned()
+        {
+            var wff = Substitute.For<IMessageWorkflowFactory>();
+
+            var mwp = new MessageWorkflowProvider(wff);
+
+            var r = mwp.CreateShoutingWorkflow();
+
+            r.Should().NotBeNull();
+        }
+
+
+        [Fact]
+        public void CreateShoutingPersonalReplyWorkflow_ResultsReturned()
+        {
+            var wff = Substitute.For<IMessageWorkflowFactory>();
+
+            var mwp = new MessageWorkflowProvider(wff);
+
+            var r = mwp.CreateShoutingPersonalReplyWorkflow();
+
+            r.Should().NotBeNull();
+        }
+
+    }
+}
