567bbf56207b8783b1df1dd6dfcab69bc59343e0
tonycknight <tonycknight@users.noreply.github.com>
2022-04-04 11:54:40 +0100

    log shouting computes

--- src/nomoretrolls/Workflows/Parts/MessageIsShoutingFilter.cs
+++ src/nomoretrolls/Workflows/Parts/MessageIsShoutingFilter.cs
@@ -1,19 +1,22 @@
 ﻿using nomoretrolls.Parsing;
+using nomoretrolls.Telemetry;
 
 namespace nomoretrolls.Workflows.Parts
 {
     internal class MessageIsShoutingFilter : IMessageWorkflowPart
     {
-        
-        public MessageIsShoutingFilter()
+        private readonly ITelemetry _telemetry;
+
+        public MessageIsShoutingFilter(ITelemetry telemetry)
         {
+            _telemetry = telemetry;
         }
 
         public Task<MessageWorkflowContext?> ExecuteAsync(MessageWorkflowContext context)
         {
             MessageWorkflowContext? result = null;
 
-            if (IsCapitals(context.Content()))
+            if (IsCapitals(context))
             {                
                 result = context;
             }
@@ -21,15 +24,20 @@ namespace nomoretrolls.Workflows.Parts
             return Task.FromResult(result);
         }
 
-        private bool IsCapitals(string content)
+        private bool IsCapitals(MessageWorkflowContext context)
         {            
+            var content = context.Content();
             if (!string.IsNullOrWhiteSpace(content))
             {
                 var letterCount = content.LetterCount();
-                
-                return letterCount >= 5 && 
-                    (content.CapitalCount() > letterCount / 2)
-                    && content.CapitalGiniImpurity() < 0.5;
+                var capitalCount = content.CapitalCount();
+                var gini = content.CapitalGiniImpurity();
+
+                _telemetry.Message($"[Message {context.DiscordContext.Message.Id}] [{this.GetType().Name}] Letters {letterCount} capitals {capitalCount} gini {gini}");
+
+                return letterCount >= 5 &&
+                    capitalCount > letterCount / 2 && 
+                    gini < 0.5;
             }
             
             return false;
