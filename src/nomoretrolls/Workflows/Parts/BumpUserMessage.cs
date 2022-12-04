f9a949462b34ce029b89c1a80b76f1b48aa30ecc
Tony Knight <tonycknight@users.noreply.github.com>
2022-12-04 22:03:27 +0000

    capture users' messaging frequency (#38)

---
+++ src/nomoretrolls/Workflows/Parts/BumpUserMessage.cs
@@ -0,0 +1,26 @@
+﻿using nomoretrolls.Statistics;
+using Tk.Extensions.Guards;
+
+namespace nomoretrolls.Workflows.Parts
+{
+    internal class BumpUserMessage : IMessageWorkflowPart
+    {
+        private IUserStatisticsProvider _statsProvider;
+        private readonly string _statsName = "user_message";
+        private readonly TimeSpan _statsExpiry = TimeSpan.FromDays(30 * 6);
+
+        public BumpUserMessage(IUserStatisticsProvider statsProvider) 
+        {
+            _statsProvider = statsProvider.ArgNotNull(nameof(statsProvider));
+        }
+
+        public async Task<MessageWorkflowContext?> ExecuteAsync(MessageWorkflowContext context)
+        {
+            var userId = context.AuthorId();
+
+            await _statsProvider.BumpUserStatisticAsync(userId, _statsName, _statsExpiry);
+
+            return context;
+        }
+    }
+}
