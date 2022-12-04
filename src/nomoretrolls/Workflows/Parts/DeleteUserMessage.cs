1be7d5d00cdb2a5f0bd041b9abce59b05207f1aa
tonycknight <tonycknight@users.noreply.github.com>
2022-12-04 16:34:17 +0000

    don't listen to DM channels

--- src/nomoretrolls/Workflows/Parts/DeleteUserMessage.cs
+++ src/nomoretrolls/Workflows/Parts/DeleteUserMessage.cs
@@ -1,12 +1,18 @@
-﻿namespace nomoretrolls.Workflows.Parts
+﻿using Discord.WebSocket;
+
+namespace nomoretrolls.Workflows.Parts
 {
     internal class DeleteUserMessage : IMessageWorkflowPart
     {
         public async Task<MessageWorkflowContext?> ExecuteAsync(MessageWorkflowContext context)
         {
             var msg = context.DiscordContext.Message;
-            // TODO: cannot delete DM messages
-            
+            // Can't delete messages from DM channels!
+            var chnl = msg.Channel as SocketDMChannel;
+            if(chnl?.Recipient != null)
+            {
+                return context;
+            }
             await msg.DeleteAsync();
 
             return context;
