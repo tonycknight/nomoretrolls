8f91e9955f3c5b0059559dd3bd98667f120fa3e9
tonycknight <tonycknight@users.noreply.github.com>
2024-07-21 17:38:58 +0100

    style

--- src/nomoretrolls/Workflows/Reactions/ShoutingReplyTextGenerator.cs
+++ src/nomoretrolls/Workflows/Reactions/ShoutingReplyTextGenerator.cs
@@ -14,7 +14,7 @@
             {
                 "Heard you the first time {0}.",
                 "The entire server heard you {0}.",
-                "Calm down {0}, calm down", 
+                "Calm down {0}, calm down",
                 "Have you gone deaf {0}?",
                 "Yell if you like {0} but if you keep it up, we'll ban you."
             };
